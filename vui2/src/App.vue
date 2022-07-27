8b5815570c69b887f54b9727f600b9d374cf0356
刘春成 <10689492+chuncheng_liu@user.noreply.gitee.com>
2022-07-27 14:02:57 +0800

    main

--- vui2/src/App.vue
+++ vui2/src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3 id="title">App根组件 {{ msg }} </h3>
+    <h3 >{{ msg }} </h3>
+    <!-- <button @click='getGoodsList'>点我啊</button> -->
+    <ul v-for="item in goodsList" :key="item.id">
+      <li>
+        商品名字: {{item.goods_name}}
+      </li>
+      <li>
+        商品图片: <img :src="item.goods_img" alt="">
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      // 存放商品列表数据
+      goodsList: [],
+      msg: '哈哈哈~'
+    }
+  },
+  methods: {
+    async getGoodsList() {
+      const { data } = await this.$http.get('https://www.escook.cn/api/cart')
+      if (data.status == 200) {
+        // 保存数据 => 渲染
+        this.goodsList = data.list
+      } else {
+        alert('获取数据失败 请稍候重试 ~')
+      }
+    }
+  },
+  beforeCreate() {
+    // console.log(this)
+    // debugger;
+  },
+  created() {
+    // console.log(this)
+    // debugger;
+    // this.getGoodsList()
+  },
+  beforeMount() {
+    // console.log(this)
+    // debugger
+  },
+  // dom挂载完毕 
+  mounted() {
+    // console.log(this.$el)
+    debugger
+    // this.getGoodsList()
+  },
+
+  // #region
+  // watch: {
+  //   goodsList: {
+  //     async handler() {
+  //       const { data } = await this.$http.get('https://www.escook.cn/api/cart')
+  //       if (data.status == 200) {
+  //         // 保存数据 => 渲染
+  //         this.goodsList = data.list
+  //       } else {
+  //         alert('获取数据失败 请稍候重试 ~')
+  //       }
+  //     },
+  //     // 页面一渲染就执行watch中的函数回调
+  //     immediate: true
+  //   }
+  // }
+  // #endregion
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+img {
+  vertical-align: top;
+}
+ul {
+  border: 2px solid gray;
+  list-style: none;
+}
+li {
+  margin-bottom: 20px;
 }
 </style>
--- vui2/src/main.js
+++ vui2/src/main.js
@@ -1,8 +1,42 @@
+// 项目入口文件
+
+// 引入vue => 仅运行时版本 => 不完整 缺少编译器 
 import Vue from 'vue'
+
+// 引入完整vue版本 => 带编译器的 => 帮助我们解析template
+// import Vue from '../node_modules/vue/dist/vue.js'
+
+// 引入App根组件 => 对所有组件进行统一的管理
 import App from './App.vue'
 
+// 引入axios
+import axios from 'axios'
+// 挂载到vue原型上 希望所有的组件实例都能访问到axios
+Vue.prototype.$http = axios
+
+// 关闭生产提示
 Vue.config.productionTip = false
 
+// eslint-disable-next-line
+// let a = 10
+
 new Vue({
+  // el: '#app',
+  // template: '<h3>App</h3>',
+
+  // 通过vue自身源码中的方法去生成元素并渲染
+  // render: (createElement)=> {
+  //   // return createElement('h3', 'my app')
+  //   return createElement(App)
+  // }
+
+  // 这种写法是上面的简写形式
   render: h => h(App),
 }).$mount('#app')
+
+
+// 关于不同版本的Vue：
+// 	1.vue.js与vue.runtime.xxx.js的区别：
+// 		(1).vue.js是完整版的Vue，包含：核心功能+模板解析器。
+// 		(2).vue.runtime.xxx.js是运行版的Vue，只包含：核心功能；没有模板解析器。
+// 	2.因为vue.runtime.xxx.js没有模板解析器，所以不能使用template配置项，需要使用render函数接收到的createElement函数去指定具体内容
